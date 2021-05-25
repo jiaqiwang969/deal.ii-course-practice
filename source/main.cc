976f14f2d350994ad11fb87c023350d37846c63f
jiaqiwang969 <jiaqiwang969@gmail.com>
2021-05-25 03:23:44 +0000

    修改BaseProblem之前的准备工作

--- source/main.cc
+++ source/main.cc
@@ -1,26 +1,10 @@
 #include <deal.II/base/utilities.h>
 
+#include "base_problem.h"      // lab10-增加基本问题
+#include "linear_elasticity.h" //  lab10-增加线性变形问题
 #include "poisson.h"
 
-/**
- * \mainpage Adaptive FEM for Poisson problem
- *
- * This is the starting code for laboratory number 7 of the course "Theory and
- * Practice of Finite Element methods".
- *
- * We solve the Poisson problem
- *
- * \[
- * \begin{split}
- * -\Delta u &= f \quad \text{ in } \Omega\\
- * n\cdot \nabla u &= g_N \quad \text{ on } \partial\Omega_N\\
- *  u &= g_D \quad \text{ on } \partial\Omega_D
- * \end{split}
- * \]
- *
- * on convex and Lipschitz domains $\Omega$, using the Finite Element method,
- * and the deal.II library (www.dealii.org).
- */
+
 int
 main(int argc, char **argv)
 {
@@ -38,9 +22,10 @@ main(int argc, char **argv)
       else
         deallog.depth_console(0);
 
-      Poisson<2> laplace_problem;
-      laplace_problem.initialize(par_name);
-      laplace_problem.run();
+
+      BaseProblem<2> base_problem;
+      base_problem.initialize(par_name);
+      base_problem.run();
     }
   catch (std::exception &exc)
     {
