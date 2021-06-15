12a28794b7b7aa139c0f91d8dd545b980d1cda7f
jiaqiwang969 <jiaqiwang969@gmail.com>
2021-06-15 08:53:55 +0000

    test pass

---
+++ build-container/gtest[1]_include.cmake
@@ -0,0 +1,5 @@
+if(EXISTS "/workspaces/deal.ii-course-practice/build-container/gtest[1]_tests.cmake")
+  include("/workspaces/deal.ii-course-practice/build-container/gtest[1]_tests.cmake")
+else()
+  add_test(gtest_NOT_BUILT gtest_NOT_BUILT)
+endif()
